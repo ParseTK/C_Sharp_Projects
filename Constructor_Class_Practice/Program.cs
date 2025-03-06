9dae1c333e7dc9e518e11a97027919cf4f3b83d8
ParseTK <tjkirchner2401@student.stcc.edu>
2025-03-06 13:32:10 -0500

    Add files via upload

---
+++ Constructor_Class_Practice/Program.cs
@@ -0,0 +1,27 @@
+﻿using Class_Constructors_Practice;
+using static System.Console;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Book book1 = new Book("Chamber of Secrets", "J.K. Rowlings", 432);
+        Book book2 = new Book("Lord of The Rings", "Unknown Author", 852);
+
+        WriteLine("Uploading books.... ");
+
+        WriteLine();
+
+        WriteLine(book1.title);
+        WriteLine(book1.author);
+        WriteLine(book1.pages);
+        
+        WriteLine();
+
+        WriteLine(book2.title);
+        WriteLine(book2.author);
+        WriteLine(book2.pages);
+
+        ReadLine();
+    }
+}
